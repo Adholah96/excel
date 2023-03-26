69cb438982a2593af5e28a48473dc510a75117a8
Adholah96 <adholah96@gmail.com>
2023-03-26 10:17:30 +0300

    Upload Excel file

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import * as XLSX from 'xlsx';
+import './App.css';
+
+function App() {
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
+        const wsName = wb.SheetNames[0];
+        const ws = wb.Sheets[wsName];
+        const data = XLSX.utils.sheet_to_json(ws);
+        resolve(data);
+      };
+      fileReader.onerror = (e) => {
+        reject(e);
+      };
+    });
+    promise.then((data) => {
+      console.log(data);
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
